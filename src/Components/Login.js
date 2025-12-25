9fbb4a685f697944e34a61a386e9cbe1561a95d0
Shreyash Ghuge <97882963+shreygh123@users.noreply.github.com>
2025-12-25 21:23:01 +0530

    Update login API endpoint to production URL

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -8,8 +8,8 @@ const Login = (props) => {
     const handleClick = async(e) => {
         e.preventDefault();
             // API Call http://localhost:5000
-            // const response = await fetch("https://inotebook-backend-gddm.onrender.com/api/auth/login", {
-            const response = await fetch("http://localhost:5000/api/auth/login", {
+            const response = await fetch("https://inotebook-backend-gddm.onrender.com/api/auth/login", {
+            // const response = await fetch("http://localhost:5000/api/auth/login", {
               method: 'POST',
               headers: {
                 'Content-Type': 'application/json',
