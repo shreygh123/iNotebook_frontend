f365210e1ff7a6b1e8f5e5e49dac9784690c230e
Shreyash Ghuge <97882963+shreygh123@users.noreply.github.com>
2026-01-02 00:16:18 +0530

    Update Login.js

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -7,7 +7,7 @@ const Login = (props) => {
     const navigate = useNavigate();
     const handleClick = async(e) => {
         e.preventDefault();
-            const response = await fetch("https://i-notebook-backend-l06pv99ct-shreyash-ghuges-projects.vercel.app/api/auth/login", {
+            const response = await fetch("https://inotebook-backend-gddm.onrender.com/api/auth/login", {
             // const response = await fetch("http://localhost:5000/api/auth/login", {
               method: 'POST',
               headers: {
